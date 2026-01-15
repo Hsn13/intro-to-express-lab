9a6a581cf6402d107b9f61ce2cf4629838d12455
Hsn13 <hkhosro5@gmail.com>
2026-01-15 22:48:21 +0300

    Testing

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head></head>
+      <body>
+        <p>Hi, my name is Hasan. I am a full-stack developer.</p>
+        <a href="/about">About me</a>
+      </body>
+    </html>
+  `)
+})
+
+app.get('/about', (req, res) => {
+  res.send(`
+    <html>
+      <head></head>
+      <body>
+        <p>I am a full-stack developer with proficiency in HTML, CSS, JavaScript, Node.js, Express.js, React.js, and MongoDB.</p>
+        <p>I have a Bachelor's degree in Computer Science from XYZ University.</p>
+        <p>I am hoping to get a job as a full-stack developer at ABC Company.</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>React.js</li>
+          <li>MongoDB</li>
+        </ul>
+      </body>
+    </html>
+  `)
+})
+
+app.get('/test', (req, res) => {
+  res.send('<p>Success!</p>')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hi, there! I am Hasan and I am developing this app!')
+})
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port} now!`)
+})
