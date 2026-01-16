782b33119dfa814cb8ce192911936efcbeb90f78
Hsn13 <hkhosro5@gmail.com>
2026-01-17 01:12:08 +0300

    Version 2

--- server.js
+++ server.js
@@ -5,39 +5,18 @@ const morgan = require("morgan");
 
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.send(`
-    <html>
-      <head></head>
-      <body>
-        <p>Hi, my name is Hasan. I am a full-stack developer.</p>
-        <a href="/about">About me</a>
-      </body>
-    </html>
-  `);
+app.use(express.static("public"));
+
+// app.get(): takes 2 arguments
+// 1. the route: /homepage
+// 2. function
+app.get("/", function (req, res) {
+  console.log(__dirname);
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/about", (req, res) => {
-  res.send(`
-    <html>
-      <head></head>
-      <body>
-        <p>I am a full-stack developer with proficiency in HTML, CSS, JavaScript, Node.js, Express.js, React.js, and MongoDB.</p>
-        <p>I have a Bachelor's degree in Programming from Polytechnic Bahrain.</p>
-        <p>I am hoping to get a job as a Software Engineer or full-stack developer at a great Company.</p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>Node.js</li>
-          <li>Express.js</li>
-          <li>React.js</li>
-          <li>MongoDB</li>
-        </ul>
-        <a href="/">Back</a>
-      </body>
-    </html>
-  `);
+app.get("/about", function (req, res) {
+  res.sendFile(__dirname + "/views/about.html");
 });
 
 app.get("/test", (req, res) => {
